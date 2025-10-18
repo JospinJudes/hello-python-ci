548074146f3a527fefdd3b4aa5413fd502a9899f
Lilacostadau <lila.costadau@gmail.com>
2025-10-18 21:05:38 +0200

    Structure following system

--- flask_auth/project/models.py
+++ flask_auth/project/models.py
@@ -4,6 +4,12 @@ from flask_login import UserMixin
 from . import db
 from datetime import datetime
 
+# Table d'association pour le système de follow (Un utilisateur peut suivre plusieurs autres utilisateurs,Un utilisateur peut être suivi par plusieurs autres)
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),# on relie ces colonnes à la colonne id de la table user
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
@@ -12,12 +18,40 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(1000))
     tweets = db.relationship('Tweet', back_populates='user', lazy='dynamic')
 
+    # Relation pour les follows
+    followed = db.relationship(
+        'User', 
+        secondary=followers, 
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),# créer automatiquement une relation inverse (followers)
+        lazy='dynamic'
+    )
 
-    #post
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
 
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user): #verifie si self suit user
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+        ).count() > 0
+    
+    def delete_follower(self,user):
+        if user in self.followers:
+            self.followers.remove(user)
+
+
+
+#post
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(280))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', back_populates='tweets')
+
